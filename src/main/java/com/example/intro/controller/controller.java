d7e6cddd748d20f650873730017274f3c2680ef4
Charan <veerankicharan4@gmail.com>
2025-05-20 15:16:16 +0530

    UC5 Updating Controller

--- src/main/java/com/example/intro/controller/controller.java
+++ src/main/java/com/example/intro/controller/controller.java
@@ -1,15 +1,44 @@
 package com.example.intro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.intro.dto.dto;
+import com.example.intro.Model.Model;
+import com.example.intro.service.Service_Interface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/addressbook")
 public class controller {
 
+    @Autowired
+    private Service_Interface service;
+
+    @PostMapping
+    public ResponseEntity<Model> add(@RequestBody dto dto) {
+        return ResponseEntity.ok(service.addAddress(dto));
+    }
+
     @GetMapping
-    public String welcome() {
-        return "Welcome to Address Book App!";
+    public ResponseEntity<List<Model>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Model> getById(@PathVariable int id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Model> update(@PathVariable int id, @RequestBody dto dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        service.delete(id);
+        return ResponseEntity.ok("Deleted ID: " + id);
     }
 }
